ca76b8ebd0f91af8c33b125397560a47d407ee65
Rolin Blake <rolinmblake@gmail.com>
2023-08-22 17:00:43 -0500

    init and cleanup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        TODO: make the thing
+      </header>
+    </div>
+  );
+}
+
+export default App;
