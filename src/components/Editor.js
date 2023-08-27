392d85d0960f2baccdef9920d7a195924d575f7f
Rolin Blake <rolinmblake@gmail.com>
2023-08-27 02:33:57 -0500

    added selections to editor as well

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -4,11 +4,173 @@ const Editor = () => {
   const [pngUrl, setPngUrl] = useState('');
   const [newPngUrl, setNewPngUrl] = useState('');
   const [warpAmount, setWarpAmount] = useState(1.25);
+  const [warpSelectionX, setWarpSelectionX] = useState('sin');
+  const [warpSelectionY, setWarpSelectionY] = useState('cos');
   const [swirlAmount, setSwirlAmount] = useState(0.02);
+  const [swirlSelectionX, setSwirlSelectionX] = useState('cos');
+  const [swirlSelectionY, setSwirlSelectionY] = useState('sin');
   const [repeatFactor, setRepeatFactor] = useState(0.02);
   const [repeatIters, setRepeatIters] = useState(0);
   const [shiftAmountX, setShiftAmountX] = useState(20);
   const [shiftAmountY, setShiftAmountY] = useState(10);
+  const [shiftSelectionX, setShiftSelectionX] = useState('sin');
+  const [shiftSelectionY, setShiftSelectionY] = useState('sin');
+
+  function fuzzyAlgorithm(imageData, width, height) {
+    const data = imageData.data.slice();
+    var warpFnX = null;
+    var warpFnY = null;
+    switch (warpSelectionX) {
+      case 'sin':
+        warpFnX = Math.sin;
+        break;
+      case 'cos':
+        warpFnX = Math.cos;
+        break;
+      case 'tan':
+        warpFnX = Math.tan;
+        break;
+      default:
+        break;
+    }
+    switch (warpSelectionY) {
+      case 'sin':
+        warpFnY = Math.sin;
+        break;
+      case 'cos':
+        warpFnY = Math.cos;
+        break;
+      case 'tan':
+        warpFnY = Math.tan;
+        break;
+      default:
+        break;
+    }
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        const originalIndex = (y * width + x) * 4;
+        const warpX = warpFnX(y / warpAmount) * warpAmount;
+        const warpY = warpFnY(x / warpAmount) * warpAmount;
+        const warpedX = Math.floor(x + warpX);
+        const warpedY = Math.floor(y + warpY);
+        if (warpedX >= 0 && warpedX < width && warpedY >= 0 && warpedY < height) {
+          const warpedIndex = (warpedY * width + warpedX) * 4;
+          data[originalIndex] = imageData.data[warpedIndex];         // r
+          data[originalIndex + 1] = imageData.data[warpedIndex + 1]; // g
+          data[originalIndex + 2] = imageData.data[warpedIndex + 2]; // b
+        }
+      }
+    }
+    for (let i = 0; i < data.length; i++) {
+      imageData.data[i] = data[i];
+    }
+    return imageData;
+  }
+
+  function swirlAlgorithm(imageData, width, height) {
+    const data = imageData.data.slice();
+    var swirlFnX = null;
+    var swirlFnY = null;
+    switch (swirlSelectionX) {
+      case 'sin':
+        swirlFnX = Math.sin;
+        break;
+      case 'cos':
+        swirlFnX = Math.cos;
+        break;
+      case 'tan':
+        swirlFnX = Math.tan;
+        break;
+      default:
+        break;
+    }
+    switch (swirlSelectionY) {
+      case 'sin':
+        swirlFnY = Math.sin;
+        break;
+      case 'cos':
+        swirlFnY = Math.cos;
+        break;
+      case 'tan':
+        swirlFnY = Math.tan;
+        break;
+      default:
+        break;
+    }
+    const centerX = width / 2;
+    const centerY = height / 2;
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        const offsetX = x - centerX;
+        const offsetY = y - centerY;
+        const distance = Math.sqrt(offsetX * offsetX + offsetY * offsetY);
+        const angle = Math.atan2(offsetY, offsetX);
+        const newAngle = angle + distance * swirlAmount;
+        const newX = centerX + swirlFnX(newAngle) * distance;
+        const newY = centerY + swirlFnY(newAngle) * distance;
+        const originalIndex = (y * width + x) * 4;
+        const newIndex = (Math.floor(newY) * width + Math.floor(newX)) * 4;
+        if (newIndex >= 0 && newIndex < data.length) {
+          data[originalIndex] = imageData.data[newIndex];         // r
+          data[originalIndex + 1] = imageData.data[newIndex + 1]; // g
+          data[originalIndex + 2] = imageData.data[newIndex + 2]; // b
+        }
+      }
+    }
+    for (let i = 0; i < data.length; i++) {
+      imageData.data[i] = data[i];
+    }
+    return imageData;
+  }
+
+  function shiftAlgorithm(imageData, width, height) {
+    const data = imageData.data.slice();
+    var shiftFnX = null;
+    var shiftFnY = null;
+    switch (shiftSelectionX) {
+      case 'sin':
+        shiftFnX = Math.sin;
+        break;
+      case 'cos':
+        shiftFnX = Math.cos;
+        break;
+      case 'tan':
+        shiftFnX = Math.tan;
+        break;
+      default:
+        break;
+    }
+    switch (shiftSelectionY) {
+      case 'sin':
+        shiftFnY = Math.sin;
+        break;
+      case 'cos':
+        shiftFnY = Math.cos;
+        break;
+      case 'tan':
+        shiftFnY = Math.tan;
+        break;
+      default:
+        break;
+    }
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        const sourceX = x + Math.round(shiftFnX(y / shiftAmountY) * shiftAmountX);
+        const sourceY = y + Math.round(shiftFnY(x / shiftAmountX) * shiftAmountY);
+        if (sourceX >= 0 && sourceX < width && sourceY >= 0 && sourceY < height) {
+          const sourceIndex = (sourceY * width + sourceX) * 4;
+          const targetIndex = (y * width + x) * 4;
+          data[targetIndex] = imageData.data[sourceIndex];         // r
+          data[targetIndex + 1] = imageData.data[sourceIndex + 1]; // g
+          data[targetIndex + 2] = imageData.data[sourceIndex + 2]; // b
+        }
+      }
+    }
+    for (let i = 0; i < data.length; i++) {
+      imageData.data[i] = data[i];
+    }
+    return imageData;
+  }
 
   const handlePngUpload = (event) => {
     event.preventDefault();
@@ -36,10 +198,10 @@ const Editor = () => {
         canvas.height = img.height;
         ctx.drawImage(img, 0, 0);
         var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        imageData = fuzzyAlgorithm(imageData, warpAmount, canvas.width, canvas.height);
-        imageData = swirlAlgorithm(imageData, swirlAmount, canvas.width, canvas.height);
+        imageData = fuzzyAlgorithm(imageData, canvas.width, canvas.height);
+        imageData = swirlAlgorithm(imageData, canvas.width, canvas.height);
         imageData = repeatAlgorithm(imageData, repeatFactor, repeatIters, canvas.width, canvas.height);
-        imageData = shiftAlgorithm(imageData, shiftAmountX, shiftAmountY, canvas.width, canvas.height);
+        imageData = shiftAlgorithm(imageData, canvas.width, canvas.height);
         ctx.putImageData(imageData, 0, 0);
         setNewPngUrl(canvas.toDataURL('image/png'));
         var downloadLink = document.getElementById('editor-download-link');
@@ -65,9 +227,41 @@ const Editor = () => {
             <h4 className='generator-param-preview'>Warp (Currently {warpAmount})</h4>
             <input className='generator-slider' type="range" value={warpAmount} onChange={e => setWarpAmount(e.target.value)} min="0" max="2.5" step="0.025" />
           </div>
+          <div className='generator-select-container'>
+            <h4 className='generator-param-preview'>Warp Function X (Currently {warpSelectionX})</h4>
+            <select className='generator-selection' id='generator-warp-selection' onChange={e => setWarpSelectionX(e.target.value)}>
+              <option value='sin'>Sine</option>
+              <option value='cos'>Cosine</option>
+              <option value='tan'>Tangent</option>
+            </select>
+          </div>
+          <div className='generator-select-container'>
+            <h4 className='generator-param-preview'>Warp Function Y (Currently {warpSelectionY})</h4>
+            <select className='generator-selection' id='generator-warp-selection' onChange={e => setWarpSelectionY(e.target.value)}>
+              <option value='sin'>Sine</option>
+              <option value='cos'>Cosine</option>
+              <option value='tan'>Tangent</option>
+            </select>
+          </div>
           <div className='generator-slider-container'>
             <h4 className='generator-param-preview'>Swirls (Currently {swirlAmount})</h4>
             <input className='generator-slider' type="range" value={swirlAmount} onChange={e => setSwirlAmount(e.target.value)} min="0" max="2" step="0.0125" />
+          </div>
+          <div className='generator-select-container'>
+            <h4 className='generator-param-preview'>Swirl Function X (Currently {swirlSelectionX})</h4>
+            <select className='generator-selection' id='generator-warp-selection' onChange={e => setSwirlSelectionX(e.target.value)}>
+              <option value='sin'>Sine</option>
+              <option value='cos'>Cosine</option>
+              <option value='tan'>Tangent</option>
+            </select>
+          </div>
+          <div className='generator-select-container'>
+            <h4 className='generator-param-preview'>Swirl Function Y (Currently {swirlSelectionY})</h4>
+            <select className='generator-selection' id='generator-warp-selection' onChange={e => setSwirlSelectionY(e.target.value)}>
+              <option value='sin'>Sine</option>
+              <option value='cos'>Cosine</option>
+              <option value='tan'>Tangent</option>
+            </select>
           </div>
           <div className='generator-slider-container'>
             <h4 className='generator-param-preview'>Repeat Factor (Currently {repeatFactor})</h4>
@@ -85,6 +279,22 @@ const Editor = () => {
             <h4 className='generator-param-preview'>Y-Shift (Currently {shiftAmountY})</h4>
             <input className='generator-slider' type='range' value={shiftAmountY} onChange={e => setShiftAmountY(e.target.value)} min='0' max='50' step='1'/>
           </div>
+          <div className='generator-select-container'>
+            <h4 className='generator-param-preview'>Shift Function X (Currently {shiftSelectionX})</h4>
+            <select className='generator-selection' id='generator-shift-selection' onChange={e => setShiftSelectionX(e.target.value)}>
+              <option value='sin'>Sine</option>
+              <option value='cos'>Cosine</option>
+              <option value='tan'>Tangent</option>
+            </select>
+          </div>
+          <div className='generator-select-container'>
+            <h4 className='generator-param-preview'>Shift Function Y (Currently {shiftSelectionY})</h4>
+            <select className='generator-selection' id='generator-shift-selection' onChange={e => setShiftSelectionY(e.target.value)}>
+              <option value='sin'>Sine</option>
+              <option value='cos'>Cosine</option>
+              <option value='tan'>Tangent</option>
+            </select>
+          </div>
         </div>
       }
       <h3 className='editor-subheader'>PNG Output</h3>
@@ -94,57 +304,6 @@ const Editor = () => {
       {newPngUrl && <img className='generator-preview' id='editor-out-preview' src={newPngUrl} alt='editor output .png' />}
     </div>
   );
-}
-
-function fuzzyAlgorithm(imageData, warpAmount, width, height) {
-  const data = imageData.data.slice();
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-      const originalIndex = (y * width + x) * 4;
-      const warpX = Math.sin(y / warpAmount) * warpAmount;
-      const warpY = Math.cos(x / warpAmount) * warpAmount;
-      const warpedX = Math.floor(x + warpX);
-      const warpedY = Math.floor(y + warpY);
-      if (warpedX >= 0 && warpedX < width && warpedY >= 0 && warpedY < height) {
-        const warpedIndex = (warpedY * width + warpedX) * 4;
-        data[originalIndex] = imageData.data[warpedIndex];         // r
-        data[originalIndex + 1] = imageData.data[warpedIndex + 1]; // g
-        data[originalIndex + 2] = imageData.data[warpedIndex + 2]; // b
-      }
-    }
-  }
-  for (let i = 0; i < data.length; i++) {
-    imageData.data[i] = data[i];
-  }
-  return imageData;
-}
-
-function swirlAlgorithm(imageData, swirlAmount, width, height) {
-  const data = imageData.data.slice();
-  const centerX = width / 2;
-  const centerY = height / 2;
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-      const offsetX = x - centerX;
-      const offsetY = y - centerY;
-      const distance = Math.sqrt(offsetX * offsetX + offsetY * offsetY);
-      const angle = Math.atan2(offsetY, offsetX);
-      const newAngle = angle + distance * swirlAmount;
-      const newX = centerX + Math.cos(newAngle) * distance;
-      const newY = centerY + Math.sin(newAngle) * distance;
-      const originalIndex = (y * width + x) * 4;
-      const newIndex = (Math.floor(newY) * width + Math.floor(newX)) * 4;
-      if (newIndex >= 0 && newIndex < data.length) {
-        data[originalIndex] = imageData.data[newIndex];         // r
-        data[originalIndex + 1] = imageData.data[newIndex + 1]; // g
-        data[originalIndex + 2] = imageData.data[newIndex + 2]; // b
-      }
-    }
-  }
-  for (let i = 0; i < data.length; i++) {
-    imageData.data[i] = data[i];
-  }
-  return imageData;
 }
 
 function repeatAlgorithm(imageData, repeatFactor, iterations, width, height) {
@@ -168,27 +327,6 @@ function repeatAlgorithm(imageData, repeatFactor, iterations, width, height) {
     for (let i = 0; i < data.length; i++) {
       imageData.data[i] = data[i];
     }
-  }
-  return imageData;
-}
-
-function shiftAlgorithm(imageData, shiftAmountX, shiftAmountY, width, height) {
-  const data = imageData.data.slice();
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-      const sourceX = x + Math.round(Math.sin(y / shiftAmountY) * shiftAmountX);
-      const sourceY = y + Math.round(Math.sin(x / shiftAmountX) * shiftAmountY);
-      if (sourceX >= 0 && sourceX < width && sourceY >= 0 && sourceY < height) {
-        const sourceIndex = (sourceY * width + sourceX) * 4;
-        const targetIndex = (y * width + x) * 4;
-        data[targetIndex] = imageData.data[sourceIndex];         // r
-        data[targetIndex + 1] = imageData.data[sourceIndex + 1]; // g
-        data[targetIndex + 2] = imageData.data[sourceIndex + 2]; // b
-      }
-    }
-  }
-  for (let i = 0; i < data.length; i++) {
-    imageData.data[i] = data[i];
   }
   return imageData;
 }
